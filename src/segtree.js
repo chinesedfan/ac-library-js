7390d85bd30b5671ca26d02b11a59940bc21eeeb
Xianming Zhong <chinesedfan@qq.com>
2022-09-08 20:29:37 +0800

    migrate source codes and tests

--- src/segtree.js
+++ src/segtree.js
@@ -7,12 +7,19 @@ function ceilPow2(n) {
 class SegTree {
     constructor(nOrArray, options) {
         const { op, e } = options
+        this.op = op
+        this.e = e
         const v = Array.isArray(nOrArray) ? nOrArray : Array(nOrArray).fill(0).map(() => e())
 
         const n = v.length
         const log = ceilPow2(n)
-        const size = 1 << this.log
+        const size = 1 << log
         const d = Array(2 * size)
+        this.n = n
+        this.log = log
+        this.size = size
+        this.d = d
+
         for (let i = 0; i < d.length; i++) {
             d[i] = e()
         }
@@ -22,28 +29,80 @@ class SegTree {
         for (let i = size - 1; i >= 1; i--) {
             this.update(i)
         }
-
-        this.n = n
-        this.log = log
-        this.size = size
-        this.d = d
-        this.options = options
     }
     set(p, x) {
+        p += this.size
+        this.d[p] = x
+        for (let i = 1; i <= this.log; i++) this.update(p >> i)
     }
     get(p) {
+        return this.d[p + this.size]
     }
     prod(l, r) {
+        const { size, d, op, e } = this
+        let sml = e(), smr = e()
+        l += size
+        r += size
+
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++])
+            if (r & 1) smr = op(d[--r], smr)
+            l >>= 1
+            r >>= 1
+        }
+        return op(sml, smr)
     }
     allProd() {
         return this.d[1]
     }
     maxRight(l, f) {
+        const { n, size, d, op, e} = this
+        if (l == n) return n
+        l += size
+        let sm = e()
+        do {
+            while (l % 2 == 0) l >>= 1
+            if (!f(op(sm, d[l]))) {
+                while (l < size) {
+                    l = (2 * l)
+                    if (f(op(sm, d[l]))) {
+                        sm = op(sm, d[l])
+                        l++
+                    }
+                }
+                return l - size
+            }
+            sm = op(sm, d[l])
+            l++
+        } while ((l & -l) != l)
+        return n
     }
     minLeft(r, f) {
+        if (r == 0) return 0
+        const { size, d, op, e} = this
+        r += size
+        let sm = e()
+        do {
+            r--
+            while (r > 1 && (r % 2)) r >>= 1
+            if (!f(op(d[r], sm))) {
+                while (r < size) {
+                    r = (2 * r + 1)
+                    if (f(op(d[r], sm))) {
+                        sm = op(d[r], sm)
+                        r--
+                    }
+                }
+                return r + 1 - size
+            }
+            sm = op(d[r], sm)
+        } while ((r & -r) != r)
+        return 0
     }
 
-    update(p) {
+    update(k) {
+        const { d, op } = this
+        d[k] = op(d[2 * k], d[2 * k + 1])
     }
 }
 
--- test/segtree.test.js
+++ test/segtree.test.js
@@ -10,9 +10,100 @@ function e() {
     return '$'
 }
 
+class NaiveSegTree {
+    constructor(nOrArray, options) {
+        const { op, e } = options
+        const d = Array.isArray(nOrArray) ? nOrArray : Array(nOrArray).fill(0).map(() => e())
+        this.n = d.length
+        this.d = d
+        this.op = op
+        this.e = e
+    }
+    set(p, x) {
+        this.d[p] = x
+    }
+    get(p) {
+        return this.d[p]
+    }
+    prod(l, r) {
+        let sum = this.e()
+        for (let i = l; i < r; i++) {
+            sum = op(sum, this.d[i])
+        }
+        return sum
+    }
+    allProd() {
+        return this.prod(0, this.n)
+    }
+    maxRight(l, f) {
+        const { n, d, op, e } = this
+        let sum = e()
+        for (let i = l; i < n; i++) {
+            sum = op(sum, d[i])
+            if (!f(sum)) return i
+        }
+        return n
+    }
+    minLeft(r, f) {
+        const { d, op, e } = this
+        let sum = e()
+        for (let i = r - 1; i >= 0; i--) {
+            sum = op(d[i], sum)
+            if (!f(sum)) return i + 1
+        }
+        return 0
+    }
+}
+
 describe('SegTree', () => {
     it('Zero', () => {
         const s = new SegTree(0, { op, e })
         expect(s.allProd()).toBe('$')
+    })
+    it('One', () => {
+        const s = new SegTree(1, { op, e })
+        expect(s.allProd()).toBe('$')
+        expect(s.get(0)).toBe('$')
+        expect(s.prod(0, 1)).toBe('$')
+        s.set(0, 'dummy')
+        expect(s.get(0)).toBe('dummy')
+        expect(s.prod(0, 0)).toBe('$')
+        expect(s.prod(0, 1)).toBe('dummy')
+        expect(s.prod(1, 1)).toBe('$')
+    })
+    it('CompareNative', () => {
+        let y
+        function leq_y(x) {
+            return x.length <= y.length
+        }
+        for (let n = 0; n < 30; n++) {
+            const seg0 = new NaiveSegTree(n, { op, e })
+            const seg1 = new SegTree(n, { op, e })
+            for (let i = 0; i < n; i++) {
+                const s = String.fromCharCode('a'.charCodeAt(0) + i)
+                seg0.set(i, s)
+                seg1.set(i, s)
+            }
+
+            for (let l = 0; l <= n; l++) {
+                for (let r = l; r <= n; r++) {
+                    expect(seg1.prod(l, r)).toEqual(seg0.prod(l, r))
+                }
+            }
+
+            for (let l = 0; l <= n; l++) {
+                for (let r = l; r <= n; r++) {
+                    y = seg1.prod(l, r)
+                    expect(seg1.maxRight(l, leq_y)).toEqual(seg0.maxRight(l, leq_y))
+                }
+            }
+
+            for (let r = 0; r <= n; r++) {
+                for (let l = 0; l <= r; l++) {
+                    y = seg1.prod(l, r)
+                    expect(seg1.minLeft(r, leq_y)).toEqual(seg0.minLeft(r, leq_y))
+                }
+            }
+        }
     })
 })
